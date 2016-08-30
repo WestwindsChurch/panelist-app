bcc2d7c2959c90c682454efd4bc7e714038c2ca8
joelworsham <joel.worsham@gmail.com>
2016-08-30 17:29:37 -0400

    Don't show text until sent

--- js/sender.js
+++ js/sender.js
@@ -9,21 +9,9 @@ $form.submit(function (e) {
     localStorage.setItem('storage-send-data', $input.val());
 
     $preview.html($input.val());
-    $preview.addClass('sent');
-
 
     $message.find('.text').html($input.val().length ? 'Sent!' : 'Text Erased!');
     $message.stop().slideDown(150).delay(2000).slideUp(300);
 
     $input.val('');
-});
-
-$input.keyup(function (e) {
-
-    if (e.which == 13) {
-        return;
-    }
-
-    $preview.removeClass('sent');
-    $preview.html($input.val());
 });
